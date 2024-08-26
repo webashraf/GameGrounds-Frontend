116cf22ebfb995a8ed5151be8e9a3e306ce71e64
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2024-08-26 23:10:25 +0600

    add add facilities functionality

--- src/Redux/api/baseApi.ts
+++ src/Redux/api/baseApi.ts
@@ -1,10 +1,18 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:5000/api",
     credentials: "include",
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
   }),
   endpoints: (builder) => ({
     getFacilities: builder.query({
@@ -24,8 +32,19 @@ export const baseApi = createApi({
         body: payload,
       }),
     }),
+    addFacilities: builder.mutation({
+      query: (payload) => ({
+        url: "/facility",
+        method: "POST",
+        body: payload,
+      }),
+    }),
   }),
 });
 
-export const { useGetFacilitiesQuery, useLoginMutation, useSignUpMutation } =
-  baseApi;
+export const {
+  useGetFacilitiesQuery,
+  useLoginMutation,
+  useSignUpMutation,
+  useAddFacilitiesMutation,
+} = baseApi;
--- src/components/Pages/AdminDashboard/FacilitiesManagement/FacilitiesAdd.tsx
+++ src/components/Pages/AdminDashboard/FacilitiesManagement/FacilitiesAdd.tsx
@@ -1,21 +1,39 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { useAddFacilitiesMutation } from "../../../../Redux/api/baseApi";
 import { Button } from "../../../ui/button";
 
 const FacilitiesAdd = () => {
+  const [addFacilities] = useAddFacilitiesMutation();
+  const defaultValue = {
+    name: "Basketball Court",
+    description: "Indoor basketball court with wooden flooring.",
+    pricePerHour: 50,
+    location: "789 Athlete St, Springfield",
+  };
   const {
     register,
     handleSubmit,
     reset,
     // watch,
     formState: { errors },
-  } = useForm<any>();
-  const onSubmit: SubmitHandler<any> = (data) => {
+  } = useForm<any>({ defaultValues: defaultValue });
+  const onSubmit: SubmitHandler<any> = async (data) => {
     console.log(data);
+
     reset();
+
+    try {
+      const res = await addFacilities(data);
+
+      console.log(res);
+      toast.success("Yaaaaaaaa");
+    } catch (err) {
+      console.log(err);
+    }
   };
 
-  // console.log(watch("example"));
   return (
     <div className=" mx-auto w-[50%] section-padding">
       <h2 className="text-7xl text-black uppercase">Facilities Add</h2>
@@ -86,20 +104,20 @@ const FacilitiesAdd = () => {
           />
         </div>
         <div className="w-full p-3 rounded-lg font-mono bg-blac">
-          <label
+          {/* <label
             className="block text-gray-700 text-xl font-bold mb-2"
             htmlFor="unique-input"
           >
             Photo
-          </label>
-          <div className="grid w-full items-center gap-1.5">
+          </label> */}
+          {/* <div className="grid w-full items-center gap-1.5">
             <input
               {...register("photo", { required: true })}
               id="picture"
               type="file"
               className="flex w-[100%] rounded-md border border-input bg-white px-3 py-3 text-sm text-gray-400 file:border-0 file:bg-transparent file:text-gray-600 file:text-sm file:font-medium"
             />
-          </div>
+          </div> */}
         </div>
 
         {errors.exampleRequired && <span>This field is required</span>}
